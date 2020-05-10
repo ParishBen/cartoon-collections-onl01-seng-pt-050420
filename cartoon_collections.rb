8a74d8fb74e54a9674a6d0e4103646694fb4f0de
Ben Parish <parishben13@gmail.com>
2020-05-10 18:13:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,8 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   i=0 
   while i<array.length 
-  cheese_types.each do |cheese| cheese == array[i]
+  cheese_types.each do |cheese| 
+    if cheese == array[i]
   return("#{array[i]}")
   i+=1 
   end
