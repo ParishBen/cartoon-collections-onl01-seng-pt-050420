67cdda178c0b5077fbedc936d8223d8dd04979c0
Ben Parish <parishben13@gmail.com>
2020-05-10 16:28:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(array)# code an argument here
  rtrnarray << ("#{array[i].capitalize}!")
  i+=1 
  end
- puts rtrnarray
+  rtrnarray
 end
 def long_planeteer_calls(array)# code an argument here
   
