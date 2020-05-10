775ac16d64547bea4065a0a337c5a55f86354e3b
Ben Parish <parishben13@gmail.com>
2020-05-10 16:37:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,9 @@ def summon_captain_planet(array)# code an argument here
   rtrnarray
 end
 def long_planeteer_calls(array)# code an argument here
-  
+  i=0 
+  while i<array.length 
+  myarr << ""
 end
 
 def find_the_cheese(array)# code an argument here
