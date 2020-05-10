a86f7207a651df76fc48d6efc83acf52b91b95b4
Ben Parish <parishben13@gmail.com>
2020-05-10 17:03:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,11 +17,18 @@ def summon_captain_planet(array)# code an argument here
 end
 def long_planeteer_calls(array)# code an argument here
   i=0 
+  myarr=[]
   while i<array.length 
   myarr << "#{array[i]}"
   i+=1 
 end
 if myarr.any?{|i| i.length>4}
+  true 
+else false 
+end 
+end
+
+  
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
