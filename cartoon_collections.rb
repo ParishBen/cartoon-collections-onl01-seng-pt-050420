7442a8ffd3c065dc4e213b23e6a28cae37274ef0
Ben Parish <parishben13@gmail.com>
2020-05-10 18:36:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |item| 
-    if cheese_types.include? (item) 
+    if cheese_types.detect? (item) 
   return("#{item}")
 else return nil
   end
