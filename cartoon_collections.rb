b4f1025a319c405322219d155e49fe0231017818
Ben Parish <parishben13@gmail.com>
2020-05-10 16:01:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ array=["jerry", "doc", "dopey", "bashful", "Grumpy"]
 def roll_call_dwarves(array)# code an argument here
   i=0 
   while i<array.length 
-  puts "#{i}""#{array[i]}"
+  puts "#{i+1} ""#{array[i]}"
   i+=1 
  end
 end
