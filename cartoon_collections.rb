6cdf67b333b007d99c8573ba3919c20791663075
Ben Parish <parishben13@gmail.com>
2020-05-10 17:18:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,8 +32,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   i=0 
-  while i<cheese_types.length 
-  holdarr<< cheese_types[i]
+  while i<array.length 
   i+=1 
 end 
 
